a5b7d951a7b630ed31566d75d0d845b444446667
ardiibas <mistersemar.ab@gmail.com>
2018-05-23 13:14:30 +0700

    add util

--- app/src/main/java/gifood/id/alarmmanager/ui/MainActivity.java
+++ app/src/main/java/gifood/id/alarmmanager/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package gifood.id.alarmmanager;
+package gifood.id.alarmmanager.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import gifood.id.alarmmanager.R;
 
 public class MainActivity extends AppCompatActivity {
 
